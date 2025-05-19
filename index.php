0e1f273fefb266e75f0cfd773e516520054ba8e6
zhimera <167600107+zh1mera@users.noreply.github.com>
2025-05-20 01:38:53 +0800

    initial commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Redirect Button</title>
+</head>
+<body>
+  <button onclick="window.location.href='welcome.php'">Go to Welcome</button>
+</body>
+</html>
